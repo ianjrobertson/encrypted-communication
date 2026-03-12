49ef8623907783c162b2ba11e91cf1756fee1490
Ian <ijr011235@gmail.com>
2026-03-12 18:29:28 +0000

    clean up and put in next steps

--- src/main.rs
+++ src/main.rs
@@ -7,35 +7,31 @@ use rsa::pkcs1v15::{Signature, VerifyingKey};
 use rsa::signature::Verifier;
 
 fn main() {
+    // sends a Hello Message
+    // parses the server response
     let (stream, public_key) = match connect().and_then(handshake) {
         Ok(result) => result,
         Err(e) => { println!("{e}"); return;}
     };
 
     println!("Successfully connected and verified connection");
+
+    loop {
+        //     reads some text from the terminal
+        //     if the text is “exit”, break from the loop
+        //     otherwise, send an Encrypted Message
+            // For every message the client sends to the server it:
+            //     Creates a new symmetric key K and a nonce
+            //     Encrypts the key K with the server’s public key
+            //     Encrypts the message with K
+            //     Sends the server an Encrypted Message message that includes the encrypted key, the nonce, and the encrypted message
+        //     parse the Server Response
+        // The server responds to an Encrypted Message with a Server Response that includes:
+
+        //     a new nonce
+        //     the message, encrypted with the same key but the new nonce
+    }
 }
-// For every message the client sends to the server it:
-
-//     Creates a new symmetric key K and a nonce
-//     Encrypts the key K with the server’s public key
-//     Encrypts the message with K
-//     Sends the server an Encrypted Message message that includes the encrypted key, the nonce, and the encrypted message
-
-// The server responds to an Encrypted Message with a Server Response that includes:
-
-//     a new nonce
-//     the message, encrypted with the same key but the new nonce
-
-// The client decrypts and prints each Encrypted Message the server sends it
-
-// sends a Hello Message
-// parses the server response
-// loops
-
-//     reads some text from the terminal
-//     if the text is “exit”, break from the loop
-//     otherwise, send an Encrypted Message
-//     parse the Server Response
 
 fn connect() -> Result<TcpStream, String> {
     let stream = TcpStream::connect("127.0.0.1:2222").map_err(|e| format!("Could not connect to server: {e}"))?;
@@ -57,7 +53,7 @@ fn handshake(mut stream: TcpStream) -> Result<(TcpStream, RsaPublicKey), String>
 
     let mut buffer: [u8; 4096] = [0; 4096];
     let bytes_read: usize = stream.read(&mut buffer).map_err(|e: std::io::Error| format!("Could not read message: {e}"))?;
-    
+
     let server_hello_json: &str = str::from_utf8(&buffer[..bytes_read]).expect("Server hello not in UTF8");
     let server_hello = HelloMessage::from_json(server_hello_json.to_string()).map_err(|e| format!("Could not parse hello message{e}"))?;
 
